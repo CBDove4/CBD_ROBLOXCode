ed1a8e67d0284c4a6387c27e35a2570c00002f07
CBDove4 <97912857+CBDove4@users.noreply.github.com>
2022-01-17 14:39:25 -0500

    Update tetrisserver9.lua

--- tetrisserver9.lua
+++ tetrisserver9.lua
@@ -1,3 +1,3 @@
 local model = require(8569990278)
+game.Workspace.Model.Unpacker.CBDove_Tetris.PieceSource.Value = game.Workspace.Model.Unpacker.CBDove_Pieces
 game.Workspace.Model.Unpacker.Disabled = false
-game.Workspace.Unpacker.CBDove_Tetris.PieceSource.Value = game.ServerStorage.CBDove_Pieces
